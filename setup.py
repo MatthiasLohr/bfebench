180ee054a653a58f1fd713cbe4f48d846a826b1b
Matthias Lohr <mail@mlohr.com>
2021-09-19 14:34:30 +0200

    fixed mypy typing

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup, find_packages  # type: ignore
+from setuptools import setup, find_packages
 
 
 with open('README.md', 'r') as fp:
