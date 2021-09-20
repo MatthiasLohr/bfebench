b3095ac956dc43bb0a2f3c94e291038f1f8da007
Matthias Lohr <mail@mlohr.com>
2021-09-20 13:09:29 +0200

    modified test data generation tool for more expressive filenames

--- tools/generate-test-data.py
+++ tools/generate-test-data.py
@@ -26,11 +26,25 @@ def random_bytes(n: int) -> bytes:
     return (b''.join(map(struct.Struct("!Q").pack, map(random.getrandbits, itertools.repeat(64, (n + 7) // 8)))))[:n]
 
 
+# method found at https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
+def sizeof_fmt(num: float, suffix: str = 'B') -> str:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            if num.is_integer():
+                return "%d%s%s" % (num, unit, suffix)
+            else:
+                return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
 random.seed(args.random_seed)
 
 for p in range(args.lower_boundary_power, args.upper_boundary_power + 1):
-    filename = os.path.join(args.target_directory, 'bfebench-test-%d.bin' % p)
     size = 2**p
+    size_human_readable = sizeof_fmt(size)
+    filename = os.path.join(args.target_directory, 'bfebench-test-%s.bin' % size_human_readable)
+
     print('Generating %s with size of %d bytes' % (filename, size))
     with open(filename, 'wb') as fp:
         remaining = size
