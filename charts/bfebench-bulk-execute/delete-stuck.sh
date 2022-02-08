903a02fc07462acce45febbfd4b2fdf7fbb12d72
Matthias Lohr <mail@mlohr.com>
2022-02-08 13:17:46 +0100

    bugfix

--- charts/bfebench-bulk-execute/delete-stuck.sh
+++ charts/bfebench-bulk-execute/delete-stuck.sh
@@ -4,7 +4,7 @@ pods=$(kubectl -n bfebench get pods | grep Running | awk '{print $1}')
 for pod in $pods ; do
   kubectl -n bfebench logs "$pod" -c bfebench
   read -p "Pod stuck/delete? [yes/No] "
-  if [ "$reply" == "yes" ] ; then
+  if [ "$REPLY" == "yes" ] ; then
     kubectl -n bfebench delete pod "$pod"
   fi
 done
